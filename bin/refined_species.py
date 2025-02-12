278fcf50e8c1087acf036a1347b6dab053c94f4d
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2025-02-12 11:02:11 +0800

    update

--- bin/refined_species.py
+++ bin/refined_species.py
@@ -5,23 +5,13 @@ from sklearn.model_selection import KFold
 
 from scipy.spatial.distance import braycurtis
 
-'''
-先对属进行KNN 挑选 选出WGS种K个属对对应的种，将该值作为标准，之后通过该值 同比例校正16S
-'''
-
 
 def bray_curtis_distance(x1, x2):
-    """
-    计算 Bray-Curtis 距离
-    """
     distance = np.sum(np.abs(x1 - x2)) / np.sum(np.abs(x1 + x2))
     return distance
 
 
 def find_k_nearest_neighbors(train_data, test_data, k):
-    """
-    使用KNN算法找到测试数据中每个样本在训练数据中的k个最近邻居
-    """
     nbrs = NearestNeighbors(n_neighbors=k, algorithm='brute', metric=bray_curtis_distance)
     nbrs.fit(train_data)
     distances, indices = nbrs.kneighbors(test_data)
@@ -29,20 +19,17 @@ def find_k_nearest_neighbors(train_data, test_data, k):
 
 
 def get_knn_genus(train_data, test_data, k):
-    '''
-    获取WGS中临近的K个属
-    '''
     distances, indices = find_k_nearest_neighbors(train_data, test_data, k)
     return distances, indices
 
 
 def get_species_by_genus(genus_neighbors_data, species_data):
     '''
-    获取K个属所对应的种
+    Get the species corresponding to K genera
     '''
-    # 在种层级中筛选出行名和K临近的行名一样的行
+
     selected_rows = species_data[species_data.index.isin(genus_neighbors_data.index)]
-    # 筛选出 species 表列名中包含 genus 表列名的列
+    # Filter out the columns whose species table names contain the genus table name
     selected_columns = [col for col in species_data.columns if
                         any(col.startswith(prefix) for prefix in genus_neighbors_data.columns)]
     return selected_rows[selected_columns]
@@ -50,15 +37,14 @@ def get_species_by_genus(genus_neighbors_data, species_data):
 
 def same_rate_cal(standard, uncal_genus):
     '''
-    同比例校正
+    Same ratio correction
     '''
-    # 通过species倒推到genus。
     df_standard_genus = standard.T.groupby(standard.columns.str.rsplit(';', n=1).str[0]).sum().T
-    result_df = pd.DataFrame(index=uncal_genus.index)  # 存放结果
-    # 遍历属 对每个属进行同比例的校正
+    result_df = pd.DataFrame(index=uncal_genus.index)  # save result
+
     for genus in uncal_genus.columns:
         sums_genus = uncal_genus[genus]
-        # 首先，检查 df_standard_genus 是否包含 genus 列
+        # check if df_standard_genus contains a genus column
         if genus in df_standard_genus.columns:
             sums_species = df_standard_genus[genus]
         else:
@@ -79,18 +65,15 @@ def same_rate_cal(standard, uncal_genus):
 
 def distance_same_samples(df_16s, df_wgs):
     all_columns = list(set(df_16s.columns) | set(df_wgs.columns))
-    # 创建新的表1和表2，确保包含相同的列和相同的顺序
+
     new_16s = df_16s.reindex(columns=all_columns, fill_value=0)
     new_wgs = df_wgs.reindex(columns=all_columns, fill_value=0)
-    # 使用rename方法将行名中的"_16S"替换为"_out",inplace=True在原始DataFrame上进行修改，而不是创建一个新的DataFrame。
-    #new_16s.rename(index=lambda x: x.replace("_16S", "_output"), inplace=True)  # CRC
-    # new_16s.rename(index=lambda x: x.replace("_16S", ""), inplace=True)# AGP
 
-    # 计算每个样本的 Bray-Curtis 距离
+    # Calculate the Bray-Curtis distance for each sample
     bray_curtis_distances = pd.DataFrame()
     index_list = new_16s.index.tolist()
     for i in range(len(index_list)):
-        # 如果数据类型不一致，尝试将其转换为数值类型
+        # If the data type is inconsistent, try to convert it to a numeric type
         if new_16s.iloc[i].dtype == object:
             new_16s.iloc[i] = pd.to_numeric(new_16s.iloc[i], errors='coerce')
         if new_wgs.iloc[i].dtype == object:
@@ -103,16 +86,15 @@ def distance_same_samples(df_16s, df_wgs):
 
 def evaluate_k(train_data, train_labels, k, cv=5):
     """
-    使用K折交叉验证评估给定k值的kNN模型性能。
+    Use K-fold cross validation to evaluate the performance of the kNN model for a given k value.
 
-    参数：
-    - train_data: 训练数据的属层级丰度矩阵
-    - train_labels: 训练数据的种层级丰度矩阵
-    - k: 当前评估的k值
-    - cv: 交叉验证的折数
+    Parameters:- train_data: genus-level abundance matrix of the training data
+                - train_labels: species-level abundance matrix of the training data
+                - k: the k value currently evaluated
+                - cv: the number of folds for cross validation
+    Return:
+        - Mean Squared Error (MSE)
 
-    返回：
-    - 平均均方误差（MSE）
     """
     kf = KFold(n_splits=cv, shuffle=True, random_state=42)
     mse_scores = []
@@ -121,10 +103,8 @@ def evaluate_k(train_data, train_labels, k, cv=5):
         X_train, X_val = train_data.iloc[train_index], train_data.iloc[val_index]
         y_train, y_val = train_labels.iloc[train_index], train_labels.iloc[val_index]
 
-        #训练KNN模型
         result = KNN(X_train, y_train, X_val, k)
 
-        # 计算距离；
         distance_16s_wgs = distance_same_samples(result,y_val)
         df_distance_score = pd.concat([df_distance_score, distance_16s_wgs])
 
@@ -134,18 +114,16 @@ def evaluate_k(train_data, train_labels, k, cv=5):
     return df_distance_score
 
 def KNN(df_wgs_genus,df_wgs_species,df_16s,k):
-    # 获取WGS中临近的K个属
     distances, indices = get_knn_genus(df_wgs_genus, df_16s, k=k)
     cal_result = pd.DataFrame()
     for i in range(len(df_16s)):
-        #print(f"样本 {i + 1} 的最近邻居索引：{indices[i]}")
-        genus_neighbors_data = df_wgs_genus.iloc[indices[i]]  # k个临近属
-        # 通过K个属找到对应的种
+        genus_neighbors_data = df_wgs_genus.iloc[indices[i]]
+
         species_neighbors_data = get_species_by_genus(genus_neighbors_data, df_wgs_species)
-        # 求species 层级的k个邻居的平均值
+
         row_mean = species_neighbors_data.mean(axis=0)
         df_average = pd.DataFrame([row_mean], index=['average'])
-        # 将上述平均值作为标准，同比例校正
+
         result_df = same_rate_cal(df_average, pd.DataFrame([df_16s.iloc[i]]))
         cal_result = pd.concat([cal_result, result_df])
 
@@ -153,34 +131,21 @@ def KNN(df_wgs_genus,df_wgs_species,df_16s,k):
 
 
 def species_cal(df_wgs_genus,df_wgs_species,df_test_16s):
-
-    # 确保训练集和测试集具有相同的属
     df_train = df_wgs_genus.reindex(columns=df_test_16s.columns, fill_value=0)
-
-    # 定义k值的范围
-    k_values = range(1, 11)  # k从1到10
-    cv = 5  # 5折交叉验证
+    k_values = range(1, 11)  # k from 1 to 10
+    cv = 5
     mse_results = pd.DataFrame();
-
-    # 定义种层级丰度矩阵（假设为df_wgs）
-    species_data = df_wgs_species
-
-    # 评估每个k值
+    species_data = df_wgs_species # Species-level abundance
+    # Evaluate each value of k
     for k in k_values:
         mse = evaluate_k(df_train, species_data, k, cv=cv)
         mse_results = pd.concat([mse_results, mse], axis=1)
         #print(f"k={k}, Cross-Validation MSE={mse:.4f}")
-
-    # 计算每个k值的平均Bray-Curtis距离
     mean_avg = mse_results.mean()
-    # 显示平均距离
-    print(mean_avg)
-    # 找到最小平均距离对应的k值
+    #print(mean_avg)
     best_k = int(mean_avg.idxmin().split('=')[-1])
     best_distance = mean_avg.min()
-    print(f"最佳k值为: {best_k}")
-
-    # 最佳k值下的矫正结果
+    print(f"The best K is: {best_k}")
     df_cal_result = KNN(df_train,species_data,df_test_16s,best_k)
     return df_cal_result
 
--- bin/rough_genus.py
+++ bin/rough_genus.py
@@ -5,83 +5,71 @@ import pandas as pd
 
 
 def normalize(df):
-    # 计算每行的和
     row_sums = df.sum(axis=1)
-    # 检查是否存在和为 0 的行
     zero_sum_rows = row_sums[row_sums == 0].index
     if len(zero_sum_rows) > 0:
-        #print(f"警告：以下行的和为 0，将被删除: {zero_sum_rows.tolist()}")
-        df = df.drop(zero_sum_rows)  # 删除和为 0 的行
-    # 归一化
+        df = df.drop(zero_sum_rows)  # Delete rows with sum equal to 0
+    # Normalization
     df_normalized = df.div(df.sum(axis=1), axis=0)
-
     return df_normalized
 
 
-"""
-    训练线性回归模型，用于校正 16S 数据。
-
-    参数:- abd_16s: 16S 丰度数据 (DataFrame)
-        - abd_wgs: WGS 丰度数据 (DataFrame)
-        - pair_count: 样本数量
-    返回:
-    - models: 训练好的模型字典，键为物种名称，值为对应的线性回归模型
-"""
 def train_step(abd_16s, abd_wgs, pair_count):
-    # 第一步：归一化 16S 和 WGS 数据
+    """
+        Train a linear regression model to correct 16S data.
+
+        Parameters: - abd_16s: 16S abundance data (DataFrame)
+                    - abd_wgs: WGS abundance data (DataFrame)
+                    - pair_count: number of samples
+        Returns:
+                - models: trained model dictionary, with the key being the species name and the value being the corresponding linear regression model
+    """
+
     abd_16s_norm = normalize(abd_16s)
     abd_wgs_norm = normalize(abd_wgs)
-
-    # 初始化模型字典
     models = {}
 
     for species in abd_16s.columns:
         species_16s = abd_16s_norm[species].values
         species_wgs = abd_wgs_norm[species].values
-        # 删除行和=0的行
-        # 删除16S=0且wgs=0
 
-        # 删除当前物种中 ref_sample == 0 且 target_sample == 0 的样本
+        # Delete samples with ref_sample == 0 and target_sample == 0 in the current species
         valid_indices = (species_16s != 0) | (species_wgs != 0)
         species_16s = species_16s[valid_indices]
         species_wgs = species_wgs[valid_indices]
 
-        # 如果样本为空，则跳过
         if len(species_16s) == 0 or len(species_wgs) == 0:
             continue
 
-        # 训练每个物种的模型
-        X = species_16s.reshape(-1, 1)  # 16S 丰度作为自变量
-        y = species_wgs  # WGS 丰度作为因变量
+        X = species_16s.reshape(-1, 1)
+        y = species_wgs
         model = LinearRegression()
         model.fit(X, y)
-        models[species] = model  # 将训练好的模型存入字典
+        models[species] = model
 
     return models
 
 
 
-"""
-    对单个样本的丰度数据进行校准。
-    参数:- abd: 单个样本的丰度数据（一维数组或 Series）
-    返回:- cal_abd: 校准后的丰度数据（一维数组）
-"""
 def calibrate(model_ols, abd):
-    # 归一化
+    """
+        Calibrate the abundance data of a single sample.
+        Parameters:- abd: Abundance data of a single sample
+        Returns:- cal_abd: Calibrated abundance data
+    """
+
     abd_sum = abd.sum()
     if abd_sum <= 0:
-        return abd  # 如果总和为 0，直接返回原始数据
-    cal_abd = abd / abd_sum  # 归一化
+        return abd
+    cal_abd = abd / abd_sum
 
-    # 校准
     for species, value in cal_abd.items():
         if species not in model_ols or value == 0:
-            continue  # 如果模型不存在或丰度为 0，跳过
-        model = model_ols[species]  # 使用物种名称查找模型
-        cal_abd[species] = max(model.predict([[value]])[0], 0)  # 预测并确保非负
-        cal_abd[species] *= abd_sum  # 恢复原始总和
+            continue
+        model = model_ols[species]
+        cal_abd[species] = max(model.predict([[value]])[0], 0)
+        cal_abd[species] *= abd_sum
 
-    # 再次归一化
     cal_abd_sum = cal_abd.sum()
     if cal_abd_sum > 0:
         cal_abd /= cal_abd_sum
@@ -89,50 +77,25 @@ def calibrate(model_ols, abd):
     return cal_abd
 
 
-"""
-处理所有样本的丰度数据。
-    参数:- input_abd: 输入丰度数据（DataFrame）
-        - model_ols: 线性回归模型字典
-        - sample_names: 样本名称列表
-    返回:- output_table: 校正后的丰度数据（DataFrame）
-"""
 def process_samples(model_ols, input_abd):
     output_table = pd.DataFrame(columns=input_abd.columns)
-    for i, row in input_abd.iterrows():  # 使用 iterrows 遍历 DataFrame
-        sample_name = row.name  # 获取样本名称
-        abd = row  # 获取当前样本的丰度数据
-        cal_abd = calibrate(model_ols, abd)  # 校准
-        output_table.loc[sample_name] = cal_abd  # 添加到输出表
+    for i, row in input_abd.iterrows():
+        sample_name = row.name
+        abd = row
+        cal_abd = calibrate(model_ols, abd)
+        output_table.loc[sample_name] = cal_abd
     return output_table
 
 
-"""
-主函数，用于测试代码。
-"""
+
+#main
 def genus_cal(train_16s,train_wgs,other_16s,pair_count):
-
-    """
-    train_16s = pd.read_csv('E:/1shen/结果整理汇总/调整代码所需数据/train16S_15.csv', encoding='utf-8',
-                            sep=',', header=0, index_col=0)
-    train_wgs = pd.read_csv('E:/1shen/结果整理汇总/调整代码所需数据/trainWGS_15.csv', encoding='utf-8',
-                            sep=',', header=0, index_col=0)
-    other_16s = pd.read_csv('E:/1shen/结果整理汇总/调整代码所需数据/待校正16s.csv', encoding='utf-8',
-                            sep=',', header=0, index_col=0)
-
-    pair_count = 15
-    """
-    # 调用训练函数
+    # train
     trained_models = train_step(train_16s, train_wgs, pair_count)
-
-    # 调用校正函数
+    # cal
     df_calibrated = process_samples(trained_models, other_16s)
-    #print("校正后的丰度数据：")
-    #print(df_calibrated)
-    # 计算每列的和，# 删除列和为 0 的列
+
     col_sum = df_calibrated.sum(axis=0)
     df_cleaned = df_calibrated.loc[:, col_sum != 0]
     return df_cleaned
-    #df_cleaned.to_csv('E:/1shen/结果整理汇总/调整代码所需数据/校正后.csv', index=True)
 
-
-# 运行主函数
